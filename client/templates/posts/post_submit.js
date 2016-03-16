e5cea74ead6b325d1969d7ced04186deed77ba25
lmlawas1 <leenseylawas2007@gmail.com>
2016-03-16 09:59:01 +0800

    Use a method to submit the post + minor errors

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,8 +6,22 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
+
+    // call the server side Meteor method postInsert
+    Meteor.call('postInsert', post, function(error, result){
+    	// alert if error
+      if(error)
+    		return alert(error.reason);
+
+      // alert if post already exists
+      if(result.postExists)
+          alert('This link has already been posted');
+
+      // re-route to the post whether it already exists or not
+    	Router.go('postPage', {_id: result.id});
+    });
     
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+    // post._id = Posts.insert(post);
+    // Router.go('postPage', post);
   }
 });
